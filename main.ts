4af93a44e4170e368bc935f80f11e597a9c5210d
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-02-21 21:41:49 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,15 @@ radio.onReceivedNumber(function (receivedNumber) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, input.acceleration(Dimension.X))
     } else if (receivedNumber == 2) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, input.acceleration(Dimension.X))
+    } else if (receivedNumber == 3) {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, input.acceleration(Dimension.X))
     }
 })
 radio.onReceivedString(function (receivedString) {
     DFRobotMaqueenPlus.mototStop(Motors.ALL)
 })
 let acceleration = 0
+let direction = 0
 let tilt = 0
 basic.showLeds(`
     . . . . .
@@ -19,6 +22,7 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     tilt = input.rotation(Rotation.Pitch)
+    direction = input.rotation(Rotation.Roll)
     acceleration = pins.map(
     tilt,
     -90,
@@ -30,7 +34,11 @@ basic.forever(function () {
         radio.sendString("a")
     } else if (tilt > 30) {
         radio.sendNumber(1)
-    } else {
+    } else if (tilt > -30) {
         radio.sendNumber(2)
+    } else if (direction > 60) {
+        radio.sendNumber(3)
+    } else {
+    	
     }
 })
